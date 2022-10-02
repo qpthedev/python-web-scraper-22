9dd1d7e1b842b730948b27a23b2026482e69a8c3
qpthedev <qpthedev@gmail.com>
2022-10-02 12:47:08 -0400

    selenium webdriver to bypass request blocking

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from requests import get
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+
+browser = webdriver.Chrome(options=options)
+
+browser.get("https://www.indeed.com/jobs?q=python")
+
+soup = BeautifulSoup(browser.page_source, "html.parser")
+job_list = soup.find("ul", class_="jobsearch-ResultsList")
+jobs = job_list.find_all("li", recursive=False)
+for job in jobs:
+    print(job)
+    print("///////////////////////")
