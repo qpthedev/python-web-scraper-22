cb7375978ee5db34ddbee62cfb9a39246d77bfc9
qpthedev <qpthedev@gmail.com>
2022-10-03 04:14:55 -0400

    scrape job postings and write csv file

--- main.py
+++ main.py
@@ -1,35 +1,19 @@
-from bs4 import BeautifulSoup
+from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from extractors.indeed import extract_indeed_jobs
 
-options = Options()
 
-browser = webdriver.Chrome(options=options)
+keyword = input("Enter search term: ")
 
-browser.get("https://www.indeed.com/jobs?q=python")
+indeed = extract_indeed_jobs(keyword)
+wwr = extract_wwr_jobs(keyword)
+jobs = indeed + wwr
 
-results = []
-soup = BeautifulSoup(browser.page_source, "html.parser")
-job_list = soup.find("ul", class_="jobsearch-ResultsList")
-jobs = job_list.find_all("li", recursive=False)
+file = open(f"{keyword}.csv", "w")
+file.write("Position,Company,Location,URL\n")
+
 for job in jobs:
-    zone = job.find("div", class_="mosaic-zone")
-    if zone == None:
-        anchor = job.select_one("h2 a")
-        title = anchor.find("span")["title"]
-        link = anchor["href"]
-        company = job.find("span", class_="companyName")
-        location = job.find("div", class_="companyLocation")
+    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
 
-        job_data = {
-            "link": f"https://www.indeed.com{link}",
-            "company": company.string,
-            "location": location.string,
-            "position": title,
-        }
 
-        results.append(job_data)
-
-for result in results:
-    print(result, "///////////\n")
+file.close()
