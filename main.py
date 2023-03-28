8beb9d469617c1e9d7887abc7755a280c5c52df8
qpthedev <qpthedev@gmail.com>
2023-03-28 03:42:01 -0400

    Export search results in csv format

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 
 app = Flask("JobScraper")
 
@@ -16,6 +17,8 @@ def home():
 @app.route("/search")
 def result():
     keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
     if keyword in db:
         jobs = db[keyword]
     else:
@@ -25,6 +28,17 @@ def result():
         jobs = wwr
         db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword not in db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
+    return send_file(f"{keyword}.csv", as_attachment=True)
 
 
 app.run("0.0.0.0")
