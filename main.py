0f83c8f90c5bbd5881b84d53f666f77b7c71671e
qpthedev <qpthedev@gmail.com>
2023-03-28 02:53:10 -0400

    Set up flask for home and search result page

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+from flask import Flask, render_template, request
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
-from extractors.indeed import extract_indeed_jobs
+
+app = Flask("JobScraper")
 
 
-keyword = input("Enter search term: ")
-
-indeed = extract_indeed_jobs(keyword)
-wwr = extract_wwr_jobs(keyword)
-jobs = indeed + wwr
-
-file = open(f"{keyword}.csv", "w")
-file.write("Position,Company,Location,URL\n")
-
-for job in jobs:
-    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
+@app.route("/")
+def home():
+    return render_template("home.html")
 
 
-file.close()
+@app.route("/search")
+def result():
+    keyword = request.args.get("keyword")
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    # jobs = indeed + wwr
+    # jobs = wwr
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+app.run("0.0.0.0")
